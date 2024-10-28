f56ff3e56924f26510d27d22f1436c4c80278ad2
Niklas Ekman <nikl.ekman@gmail.com>
2024-10-28 12:09:27 +0100

    Add vector extensions

--- Runtime/Extensions/Vector2Extension.cs
+++ Runtime/Extensions/Vector2Extension.cs
@@ -6,5 +6,11 @@ namespace Nekman.Core.Extensions
     {
         public static Vector2 SetX(this Vector2 v, float x) => new(x, v.y);
         public static Vector2 SetY(this Vector2 v, float y) => new(v.x, y);
+        public static Vector3 XY(this Vector3 v) => new(v.x, v.y, 0);
+        public static Vector3 XZ(this Vector3 v) => new(v.x, 0, v.y);
+        public static Vector3 YX(this Vector3 v) => new(v.x, v.y, 0);
+        public static Vector3 YZ(this Vector3 v) => new(0, v.x, v.y);
+        public static Vector3 ZX(this Vector3 v) => new(v.y, 0, v.x);
+        public static Vector3 ZY(this Vector3 v) => new(0, v.y, v.x);
     }
 }
--- Runtime/Extensions/Vector3Extension.cs
+++ Runtime/Extensions/Vector3Extension.cs
@@ -10,5 +10,15 @@ namespace Nekman.Core.Extensions
         public static Vector3 SetXY(this Vector3 v, float x, float y) => new(x, y, v.z);
         public static Vector3 SetXZ(this Vector3 v, float x, float z) => new(x, v.y, z);
         public static Vector3 SetYZ(this Vector3 v, float y, float z) => new(v.x, y, z);
+        public static Vector2 XY(this Vector3 v) => new(v.x, v.y);
+        public static Vector2 XZ(this Vector3 v) => new(v.x, v.z);
+        public static Vector2 XX(this Vector3 v) => new(v.x, v.x);
+
+        public static Vector2 YX(this Vector3 v) => new(v.y, v.x);
+        public static Vector2 YZ(this Vector3 v) => new(v.y, v.z);
+        public static Vector2 YY(this Vector3 v) => new(v.y, v.y);
+        public static Vector2 ZX(this Vector3 v) => new(v.z, v.x);
+        public static Vector2 ZY(this Vector3 v) => new(v.z, v.y);
+        public static Vector2 ZZ(this Vector3 v) => new(v.z, v.z)
     }
 }
