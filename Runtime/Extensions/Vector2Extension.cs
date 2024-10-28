cedcc958320112ab0c7fd640fcdcbcdba826fa79
Niklas Ekman <nikl.ekman@gmail.com>
2024-10-28 18:20:23 +0100

    Missing colon

--- Runtime/Extensions/Vector2Extension.cs
+++ Runtime/Extensions/Vector2Extension.cs
@@ -9,7 +9,7 @@ namespace Nekman.Core.Extensions
         public static Vector3 XY(this Vector3 v, float z = 0) => new(v.x, v.y, z);
         public static Vector3 XZ(this Vector3 v, float y = 0) => new(v.x, y, v.y);
         public static Vector3 YX(this Vector3 v, float z = 0) => new(v.y, v.x, z);
-        public static Vector3 YZ(this Vector3 v, float x = 0) => new(x, v.y v.x);
+        public static Vector3 YZ(this Vector3 v, float x = 0) => new(x, v.y, v.x);
         public static Vector3 ZX(this Vector3 v, float y = 0) => new(v.y, y, v.x);
         public static Vector3 ZY(this Vector3 v, float x = 0) => new(x, v.x, v.y);
     }
