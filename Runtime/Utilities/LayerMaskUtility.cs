88742960f5d2cefbe58146481c3000ef7bf1e264
Niklas Ekman <nikl.ekman@gmail.com>
2024-11-26 18:01:34 +0100

    Add LayerMaskUtility

---
+++ Runtime/Utilities/LayerMaskUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Nekman.Core.Utilities
+{
+    public static class LayerMaskUtility
+    {
+        public static int Combine(IEnumerable<LayerMask> layerMasks)
+            => layerMasks
+                .Select(layerMask => layerMask.value)
+                .Aggregate(
+                    0,
+                    (combinedMask, layerMask) => combinedMask | layerMask
+                );
+    }
+}
