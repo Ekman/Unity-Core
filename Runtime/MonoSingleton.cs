f150e62e504e2269f9ed587311ab39c2b5856b44
Niklas Ekman <nikl.ekman@gmail.com>
2025-05-17 18:50:15 +0200

    Add MonoSingleton

---
+++ Runtime/MonoSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Nekman.Core
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance { get; private set; }
+
+        public void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
