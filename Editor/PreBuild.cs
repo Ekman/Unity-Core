14b82060eb61bbaaccc49d1e2de8358fb37db37f
Niklas Ekman <nikl.ekman@gmail.com>
2025-05-19 11:42:43 +0200

    Add OcclussionCulling to build script1

---
+++ Editor/PreBuild.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace Nekman.Core.Editor
+{
+    public static class PreBuild
+    {
+        public static void Run()
+        {
+            StaticOcclusionCulling.GenerateInBackground();
+        }
+    }
+}
