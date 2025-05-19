c53320b22cd09e2c2bc32697426b1ad5599698ef
Niklas Ekman <nikl.ekman@gmail.com>
2025-05-19 11:29:47 +0200

    Do not LookRotation if vector is empty

--- Runtime/Extensions/RigidbodyExtension.cs
+++ Runtime/Extensions/RigidbodyExtension.cs
@@ -4,10 +4,15 @@ namespace Nekman.Core.Extensions
 {
     public static class RigidbodyExtension
     {
-        public static void LookRotation(this Rigidbody body, Vector3 forward, Vector3 up) 
-            => body.MoveRotation(
-                Quaternion.LookRotation(forward, up)
-            );
+        public static void LookRotation(this Rigidbody body, Vector3 forward, Vector3 up)
+        {
+            if (forward != Vector3.zero)
+            {
+                body.MoveRotation(
+                    Quaternion.LookRotation(forward, up)
+                );   
+            }
+        }
 
         public static void LookRotation(this Rigidbody body, Vector3 forward)
             => LookRotation(body, forward, Vector3.up);
