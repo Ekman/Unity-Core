d4906a04d39c240b677401d3bd47a25640c732ed
Niklas Ekman <nikl.ekman@gmail.com>
2025-05-21 18:55:54 +0200

    Add utility to draw ray all directions

---
+++ Runtime/Utilities/DebugUtility.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Nekman.Core.Utilities {
+    public static class DebugUtility {
+        [Conditional("UNITY_EDITOR")]
+        public static void DrawRayAllDirections(Vector3 start, Color color) {
+            var allDirections = new Vector3[] {
+                Vector3.up,
+                Vector3.down,
+                Vector3.left,
+                Vector3.right,
+                Vector3.forward,
+                Vector3.back,
+            };
+
+            foreach (var direction in allDirections) {
+                Debug.DrawRay(start, direction, color);   
+            }
+        }
+    }
+}
