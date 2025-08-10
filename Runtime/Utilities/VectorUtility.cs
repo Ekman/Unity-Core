176b0ff9e4847ca3ce92f861a01e8411aaade465
Niklas Ekman <nikl.ekman@gmail.com>
2025-08-10 13:45:36 +0200

    Add GetHitPoint function

--- Runtime/Utilities/VectorUtility.cs
+++ Runtime/Utilities/VectorUtility.cs
@@ -5,5 +5,13 @@ namespace Nekman.Core.Utilities
     public static class VectorUtility
     {
         public static Vector3 GetDirectionTo(Vector3 origin, Vector3 target) => (target - origin).normalized;
+        
+        /// <summary>
+        /// Calculate where an attack would land, given a point of attack and direction and max distance.
+        /// </summary>
+        /// <param name="attack"></param>
+        /// <param name="maxDistance"></param>
+        /// <returns></returns>
+        public static Vector3 GetHitPoint(Vector3 attackDirection, float maxDistance) => attack.position + attack.position.normalized * maxDistance;
     }
 }
