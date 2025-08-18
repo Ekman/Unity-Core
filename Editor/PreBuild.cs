78761039210233331f11931bc347f663f219ebeb
Niklas Ekman <nikl.ekman@gmail.com>
2025-08-18 15:36:28 +0200

    Build addressables

--- Editor/PreBuild.cs
+++ Editor/PreBuild.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
 
 namespace Nekman.Core.Editor
 {
@@ -6,7 +9,15 @@ namespace Nekman.Core.Editor
     {
         public static void Run()
         {
+            Debug.Log("Building addressable.");
+            AddressableAssetSettingsDefaultObject.Settings.ActivePlayerDataBuilder.ClearCachedData();
+            AddressableAssetSettings.CleanPlayerContent();
+            AddressableAssetSettings.BuildPlayerContent();
+            
+            Debug.Log("Baking lightning.");
             Lightmapping.Bake();
+            
+            Debug.Log("Baking occlussion culling.");
             StaticOcclusionCulling.GenerateInBackground();
         }
     }
