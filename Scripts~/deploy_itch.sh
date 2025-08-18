e208924dda0d048e96430cfdd987659765e9d238
Niklas Ekman <nikl.ekman@gmail.com>
2025-08-18 21:10:25 +0200

    Update deploy_itch.sh

--- Scripts~/deploy_itch.sh
+++ Scripts~/deploy_itch.sh
@@ -7,8 +7,8 @@ build_artifact_path="./Builds"
 version=""
 platforms=""
 
-project_name="It's Not the End of the World"
-project_name_slug="its-not-the-end-of-the-world"
+project_name="ItsNottheEndoftheWorld"
+itch_project_name="its-not-the-end-of-the-world"
 
 while getopts "u:o:d:v:p:" flag
 do
@@ -41,19 +41,20 @@ for platform in "${split_platforms[@]}"; do
     echo "Deploying $platform..."
 
     arch="x64"
-    platform_lower="$platform"
+    platform_lower="$(echo "$platform" | tr "[:upper:]" "[:lower:]")"
     platform_path="$build_artifact_path/$platform"
     manifest_path="$platform_path/.itch.toml"
 
-    if [[ "$platform" = "OsxSilicon" ]]; then
+    if [[ "$platform" = "OsxUniversal" ]]; then
+        arch=""
+        platform_lower="osx"
+    elif [[ "$platform" = "OsxSilicon" ]]; then
         arch="arm64"
         platform_lower="osx"
     elif [[ "$platform" = "OsxIntel" ]]; then
         platform_lower="osx"
     elif [[ "$platform" = "WebGl" ]]; then
         platform_lower="html"
-    else
-        platform_lower="$(echo "$platform" | tr "[:upper:]" "[:lower:]")"
     fi
 
     # See: https://itch.io/docs/itch/integrating/manifest-actions.html
@@ -65,17 +66,21 @@ for platform in "${split_platforms[@]}"; do
         extension="exe"
     fi
 
-    tag="$platform_lower-$arch"
-    if [[ "$platform" = "WebGl" ]]; then
-        tag="html"
+    if [[ "$platform" = "OsxUniversal" ]] || [[ "$platform" = "WebGl" ]]; then
+        tag="$platform_lower"
     else
+        tag="$platform_lower-$arch"
+    fi
+
+    # Generate itch.toml for platforms except WebGL
+    if [[ "$platform" != "WebGl" ]]; then
         echo "[[actions]]
         name = \"play\"
         platform = \"$platform_lower\"
         path = \"$project_name.$extension\"
         sandbox = true" 1> "$manifest_path"
 
-        "$butler_path" validate --assume-yes  "$platform_path"
+        "$butler_path" validate --assume-yes "$platform_path"
     fi
 
     "$butler_path" push \
@@ -90,7 +95,7 @@ for platform in "${split_platforms[@]}"; do
         --ignore "*.zip" \
         --ignore "*.tar" \
         --ignore "*.tar.gz" \
-        "$build_artifact_path/$platform" braska/$project_name_slug:"$tag"
+        "$build_artifact_path/$platform" braska/$itch_project_name:"$tag"
 
     echo "$platform done!"
 done
