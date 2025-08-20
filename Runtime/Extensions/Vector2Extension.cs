395df00f427748fd984f814bdecdece7f8a448a5
Niklas Ekman <nikl.ekman@gmail.com>
2025-08-20 23:24:51 +0200

    Add Vector3 functions to set XY, XX, etc to one and same value

--- Runtime/Extensions/Vector2Extension.cs
+++ Runtime/Extensions/Vector2Extension.cs
@@ -4,13 +4,13 @@ namespace Nekman.Core.Extensions
 {
     public static class Vector2Extension
     {
-        public static Vector2 SetX(this Vector2 v, float x) => new(x, v.y);
-        public static Vector2 SetY(this Vector2 v, float y) => new(v.x, y);
-        public static Vector3 XY(this Vector3 v, float z = 0) => new(v.x, v.y, z);
-        public static Vector3 XZ(this Vector3 v, float y = 0) => new(v.x, y, v.y);
-        public static Vector3 YX(this Vector3 v, float z = 0) => new(v.y, v.x, z);
-        public static Vector3 YZ(this Vector3 v, float x = 0) => new(x, v.y, v.x);
-        public static Vector3 ZX(this Vector3 v, float y = 0) => new(v.y, y, v.x);
-        public static Vector3 ZY(this Vector3 v, float x = 0) => new(x, v.x, v.y);
+        public static Vector2 SetX(this Vector2 vector, float x) => new(x, vector.y);
+        public static Vector2 SetY(this Vector2 vector, float y) => new(vector.x, y);
+        public static Vector3 XY(this Vector3 vector, float z = 0) => new(vector.x, vector.y, z);
+        public static Vector3 XZ(this Vector3 vector, float y = 0) => new(vector.x, y, vector.y);
+        public static Vector3 YX(this Vector3 vector, float z = 0) => new(vector.y, vector.x, z);
+        public static Vector3 YZ(this Vector3 vector, float x = 0) => new(x, vector.y, vector.x);
+        public static Vector3 ZX(this Vector3 vector, float y = 0) => new(vector.y, y, vector.x);
+        public static Vector3 ZY(this Vector3 vector, float x = 0) => new(x, vector.x, vector.y);
     }
 }
--- Runtime/Extensions/Vector3Extension.cs
+++ Runtime/Extensions/Vector3Extension.cs
@@ -4,20 +4,23 @@ namespace Nekman.Core.Extensions
 {
     public static class Vector3Extension
     {
-        public static Vector3 SetX(this Vector3 v, float x) => new(x, v.y, v.z);
-        public static Vector3 SetY(this Vector3 v, float y) => new(v.x, y, v.z);
-        public static Vector3 SetZ(this Vector3 v, float z) => new(v.x, v.y, z);
-        public static Vector3 SetXY(this Vector3 v, float x, float y) => new(x, y, v.z);
-        public static Vector3 SetXZ(this Vector3 v, float x, float z) => new(x, v.y, z);
-        public static Vector3 SetYZ(this Vector3 v, float y, float z) => new(v.x, y, z);
-        public static Vector2 XY(this Vector3 v) => new(v.x, v.y);
-        public static Vector2 XZ(this Vector3 v) => new(v.x, v.z);
-        public static Vector2 XX(this Vector3 v) => new(v.x, v.x);
-        public static Vector2 YX(this Vector3 v) => new(v.y, v.x);
-        public static Vector2 YZ(this Vector3 v) => new(v.y, v.z);
-        public static Vector2 YY(this Vector3 v) => new(v.y, v.y);
-        public static Vector2 ZX(this Vector3 v) => new(v.z, v.x);
-        public static Vector2 ZY(this Vector3 v) => new(v.z, v.y);
-        public static Vector2 ZZ(this Vector3 v) => new(v.z, v.z);
+        public static Vector3 SetX(this Vector3 vector, float x) => new(x, vector.y, vector.z);
+        public static Vector3 SetY(this Vector3 vector, float y) => new(vector.x, y, vector.z);
+        public static Vector3 SetZ(this Vector3 vector, float z) => new(vector.x, vector.y, z);
+        public static Vector3 SetXY(this Vector3 vector, float x, float y) => new(x, y, vector.z);
+        public static Vector3 SetXY(this Vector3 vector, float v) => new(v, v, vector.z);
+        public static Vector3 SetXZ(this Vector3 vector, float x, float z) => new(x, vector.y, z);
+        public static Vector3 SetXZ(this Vector3 vector, float v) => new(v, vector.y, v);
+        public static Vector3 SetYZ(this Vector3 vector, float y, float z) => new(vector.x, y, z);
+        public static Vector3 SetYZ(this Vector3 vector, float v) => new(vector.x, v, v);
+        public static Vector2 XY(this Vector3 vector) => new(vector.x, vector.y);
+        public static Vector2 XZ(this Vector3 vector) => new(vector.x, vector.z);
+        public static Vector2 XX(this Vector3 vector) => new(vector.x, vector.x);
+        public static Vector2 YX(this Vector3 vector) => new(vector.y, vector.x);
+        public static Vector2 YZ(this Vector3 vector) => new(vector.y, vector.z);
+        public static Vector2 YY(this Vector3 vector) => new(vector.y, vector.y);
+        public static Vector2 ZX(this Vector3 vector) => new(vector.z, vector.x);
+        public static Vector2 ZY(this Vector3 vector) => new(vector.z, vector.y);
+        public static Vector2 ZZ(this Vector3 vector) => new(vector.z, vector.z);
     }
 }
